5c323ac5cab112fa401e50348876e83f5e6deea9
madcowken <adarshmelethil@gmail.com>
2019-02-17 11:28:46 -0700

    fix mistatke

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -102,7 +102,7 @@ def gendata():
     "Somebody stole my car. Car,car,car,car,stole,stole,stole,stole.",
   ]
 
-  # all_data = []
+  all_data = []
   # for result in perdelta(time_limit[0], time_limit[1], datetime.timedelta(hours=1)):
   #   # for i in range(2):
   #   if random.uniform(0,1) < 0.5:
